15bc7a49b2b0057b610d79a362682ff94a23c3eb
Shubhanshu Soni <shubhanshu.soni@qcin.org>
2018-01-04 20:07:40 +0530

    Done

--- DevOps/dockerbuild.sh
+++ DevOps/dockerbuild.sh
@@ -7,10 +7,10 @@ REPONAME="collect-assesor-frontend-pipeline"
 
 # git clone $GITHUB_LINK
 chgrp -R docker /var/lib/jenkins/workspace/collect-assesor-frontend-pipeline/dist
-# mv /var/lib/jenkins/workspace/collect-assesor-frontend/dist/ /home/ubuntu/dist_backups/collect/dist_$TIMESTAMP
+mv /var/lib/jenkins/workspace/collect-assesor-frontend/dist/ /home/ubuntu/dist_backups/collect/dist_$TIMESTAMP
 mv /var/lib/jenkins/workspace/collect-assesor-frontend-pipeline/dist /var/lib/jenkins/workspace/collect-assesor-frontend/
-# rm -rf $REPONAME
-# docker rm $DOCKERNAME
-# docker rmi $IMAGENAME
+rm -rf $REPONAME
+docker rm $DOCKERNAME
+docker rmi $IMAGENAME
 docker build -t $IMAGENAME /var/lib/jenkins/workspace/collect-assesor-frontend/.
 docker run -dt --name $DOCKERNAME $IMAGENAME
