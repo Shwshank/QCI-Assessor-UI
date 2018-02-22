7224dd4059544e9cc0e53f3a19f598252f8257a3
Shwshank <shwshank3@gmail.com>
2018-02-22 19:38:54 +0530

    notification test7

--- src/app/service/ProjectService.ts
+++ src/app/service/ProjectService.ts
@@ -79,6 +79,15 @@ export class ProjectService {
           },6000);
       });
 
+      navigator.serviceWorker.register('sw.js');
+      Notification.requestPermission(function(result) {
+        if (result === 'granted') {
+          navigator.serviceWorker.ready.then(function(registration) {
+            registration.showNotification('Notification with ServiceWorker');
+          });
+        }
+      });
+
       this.router.navigate(['./']);
 
     }
