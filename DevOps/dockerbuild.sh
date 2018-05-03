71a8a2ace3ee195723aaaf1d30e0fa39d5d83d1c
Shubhanshu Soni <shubhanshu.soni@qcin.org>
2018-05-03 16:28:12 +0530

    Corrected REPONAME

--- DevOps/dockerbuild.sh
+++ DevOps/dockerbuild.sh
@@ -3,12 +3,12 @@ TIMESTAMP=`TZ=IST-5:30 date +%F-%H%M`
 GITHUB_LINK="https://github.com/Shwshank/QCI-Assessor-UI.git"
 DOCKERNAME="Collect-Assesor-Frontend"
 IMAGENAME="qci-collect-assesor-front"
-REPONAME="collect-assesor-frontend-pipeline"
+REPONAME="QCI-Assessor-UI"
 
 git clone $GITHUB_LINK
 # chgrp -R docker /var/lib/jenkins/workspace/collect-assesor-frontend-pipeline/dist
 # mv /var/lib/jenkins/workspace/collect-assesor-frontend/dist/ /home/ubuntu/dist_backups/collect/dist_$TIMESTAMP
-mv /var/lib/jenkins/workspace/collect-assesor-frontend-pipeline/dist /var/lib/jenkins/workspace/collect-assesor-frontend/
+mv /var/lib/jenkins/workspace/collect-assesor-frontend-pipeline/QCI-Assessor-UI/dist /var/lib/jenkins/workspace/collect-assesor-frontend/
 rm -rf $REPONAME
 docker stop $DOCKERNAME
 docker rm $DOCKERNAME
